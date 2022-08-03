5a1e1fa37244bda86c965b2c75c1cc5abee0e02c
h-a-n-a <andywangsy@gmail.com>
2022-08-04 00:28:16 +0800

    feat: add more cases for inline matchResource

--- examples/inline-match-resource/extract-style-loader/loader.js
+++ examples/inline-match-resource/extract-style-loader/loader.js
@@ -7,7 +7,12 @@ module.exports = function (source) {
 		return `import ${JSON.stringify(
 			this.utils.contextify(
 				this.context || this.rootContext,
-				`${this.resource}.css!=!${getStylesLoader}!${this.remainingRequest}`
+				// `${this.resource}.css!=!${getStylesLoader}!${this.remainingRequest}`
+				// This will create a css file(this is a default behavior for expr.css impl) with module type of 'css'
+				`${this.resource}.webpack[css]!=!${getStylesLoader}!${this.remainingRequest}`
+				// This will create an svg file(default behavior for asset/resource) with module type of 'asset/resource'
+				// which solves https://github.com/webpack/webpack/issues/14851
+				// `${this.resource}.svg.webpack[asset/resource]!=!${getStylesLoader}!${this.remainingRequest}`
 			)
 		)};${source}`;
 	}
